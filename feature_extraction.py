0261eff3baa99c77b87d147a7682da3ae69b4eb5
Garrick <lst19941027@gmail.com>
2019-02-02 19:58:31 +1100

    feature extractor class

--- feature_extraction.py
+++ feature_extraction.py
@@ -40,12 +40,16 @@ class InputFeatures(object):
   """A single set of features of data."""
 
   def __init__(self,
+               tokens,
                input_ids,
                input_mask,
-               segment_ids):
+               segment_ids,
+               seq_length):
     self.input_ids = input_ids
     self.input_mask = input_mask
     self.segment_ids = segment_ids
+    self.tokens = tokens
+    self.seq_length = seq_length
 
 class ComputeGraph:
 
@@ -65,82 +69,87 @@ class ComputeGraph:
         self.outputs = self.model.get_sequence_output()
 
 
-def create_feature(text, tokenizer, max_seq_length):
-    tokens_a = tokenizer.tokenize(text)
-    tokens_b = None
+class FeatureExtractor:
+    def __init__(self,  FLAGS):
+        self.FLAGS = FLAGS
+        self.tokenizer = tokenization.FullTokenizer(
+            vocab_file=self.FLAGS.vocab_file, do_lower_case=self.FLAGS.do_lower_case)
+        bert_config = modeling.BertConfig.from_json_file(self.FLAGS.bert_config_file)
+        self.build_graph(bert_config, self.FLAGS.max_seq_length, self.FLAGS.use_tpu)
+        self.restore_model(self.FLAGS.init_checkpoint)
 
-    tokens = []
-    segment_ids = []
-    tokens.append("[CLS]")
-    segment_ids.append(0)
-    for token in tokens_a:
-        tokens.append(token)
+
+    def build_graph(self, bert_config, max_seq_length, use_one_hot_embeddings, is_training=False):
+        self.graph = ComputeGraph(bert_config, max_seq_length, use_one_hot_embeddings, is_training)
+
+    def restore_model(self, init_check_point):
+        saver = tf.train.Saver()
+        self.sess = tf.Session()
+        saver.restore(self.sess, init_check_point)
+
+    def create_feature(self, text, tokenizer, max_seq_length):
+        tokens_a = tokenizer.tokenize(text)
+        tokens_b = None
+
+        tokens = []
+        segment_ids = []
+        tokens.append("[CLS]")
         segment_ids.append(0)
-    tokens.append("[SEP]")
-    segment_ids.append(0)
-
-    if tokens_b:
-        for token in tokens_b:
+        for token in tokens_a:
             tokens.append(token)
-            segment_ids.append(1)
+            segment_ids.append(0)
         tokens.append("[SEP]")
-        segment_ids.append(1)
-
-    input_ids = tokenizer.convert_tokens_to_ids(tokens)
-    input_mask = [1] * len(input_ids)
-
-    while len(input_ids) < max_seq_length:
-        input_ids.append(0)
-        input_mask.append(0)
         segment_ids.append(0)
 
-    assert len(input_ids) == max_seq_length
-    assert len(input_mask) == max_seq_length
-    assert len(segment_ids) == max_seq_length
+        if tokens_b:
+            for token in tokens_b:
+                tokens.append(token)
+                segment_ids.append(1)
+            tokens.append("[SEP]")
+            segment_ids.append(1)
 
-    input_ids = np.asarray(input_ids).reshape(1, max_seq_length)
-    input_mask = np.asarray(input_mask).reshape(1, max_seq_length)
-    segment_ids = np.asarray(segment_ids).reshape(1, max_seq_length)
+        seq_length = len(tokens)
 
-    return InputFeatures(input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids)
+        input_ids = tokenizer.convert_tokens_to_ids(tokens)
+        input_mask = [1] * len(input_ids)
 
-def build_graph(bert_config, max_seq_length, use_one_hot_embeddings, is_training=False):
-    graph = ComputeGraph(bert_config, max_seq_length, use_one_hot_embeddings, is_training)
-    return graph
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
 
-if __name__ == "__main__":
-    tokenizer = tokenization.FullTokenizer(
-        vocab_file=FLAGS.vocab_file, do_lower_case=FLAGS.do_lower_case)
-    bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
-    graph = build_graph(bert_config, FLAGS.max_seq_length, FLAGS.use_tpu)
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
 
-        saver.restore(sess, FLAGS.init_checkpoint)
+        input_ids = np.asarray(input_ids).reshape(1, max_seq_length)
+        input_mask = np.asarray(input_mask).reshape(1, max_seq_length)
+        segment_ids = np.asarray(segment_ids).reshape(1, max_seq_length)
 
-        tvars = tf.trainable_variables()
-        initialized_variable_names = {}
+        return InputFeatures(tokens=tokens, input_ids=input_ids, input_mask=input_mask,
+                             segment_ids=segment_ids, seq_length=seq_length)
 
-        (assignment_map, initialized_variable_names
-         ) = modeling.get_assignment_map_from_checkpoint(tvars, FLAGS.init_checkpoint)
+    def map_tokens_vectors(self, input_features, output_vectors):
+        map_token_vector = []
+        for i in range(input_features.seq_length):
+            token_vector_pair = (input_features.tokens[i], output_vectors[0, i, :].tolist())
+            map_token_vector.append(token_vector_pair)
+        return map_token_vector
 
-        print("**** Trainable Variables ****")
-        for var in tvars:
-            init_string = ""
-            if var.name in initialized_variable_names:
-                init_string = ", *INIT_FROM_CKPT*"
-                print("  name = %s, shape = %s%s", var.name, var.shape,
-                            init_string)
+    def get_features(self, text):
+        input_features = self.create_feature(text, self.tokenizer, self.FLAGS.max_seq_length)
+        outputs = self.sess.run(self.graph.outputs, feed_dict={self.graph.input_ids: input_features.input_ids,
+                                                               self.graph.input_mask: input_features.input_mask,
+                                                               self.graph.segment_ids: input_features.segment_ids})
+        map_token_vector = self.map_tokens_vectors(input_features, outputs)
+        return map_token_vector
 
-        while True:
-            input_text = input("Input the text:")
-            if input_text == 'q':
-                break
-            input_features = create_feature(input_text, tokenizer, FLAGS.max_seq_length)
-            outputs = graph.outputs.eval(feed_dict={graph.input_ids: input_features.input_ids,
-                                                       graph.input_mask: input_features.input_mask,
-                                                       graph.segment_ids: input_features.segment_ids})
-            print(outputs)
+    def end_session(self):
+        self.sess.close()
+
+
+def get_init_config():
+    return FLAGS
 
 
 
